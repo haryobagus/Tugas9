4b7e1cd078646116dbdb9cc092e1d721bb5f444e
haryobagus <haryobagus14@gmail.com>
2019-10-24 15:59:08 +0700

    Soal No 1 fix

--- Soal1/app/src/main/java/com/example/soal1/MainActivity.java
+++ Soal1/app/src/main/java/com/example/soal1/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.soal1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.soal1.adapters.SuperHeroAdapter;
+import com.example.soal1.models.SuperHero;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    //instansiasi Recyclerview
+    RecyclerView rvSuperHero;
+    //instansiasi list superhero
+    List<SuperHero> listSuperHero = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rvSuperHero = findViewById(R.id.rvSuperHero);
+        SuperHero hero = new SuperHero("Superman");
+        listSuperHero.add(hero);
+        SuperHeroAdapter superHeroAdapter = new SuperHeroAdapter(listSuperHero);
+        rvSuperHero.setAdapter(superHeroAdapter);
+        rvSuperHero.setLayoutManager(new LinearLayoutManager(this));
     }
 }
+
---
+++ Soal1/app/src/main/java/com/example/soal1/models/SuperHero.java
@@ -0,0 +1,20 @@
+package com.example.soal1.models;
+
+public class SuperHero {
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
+    }
+
+    public String heroName;
+
+
+    public SuperHero(String heroName) {
+        this.heroName = heroName;
+
+    }
+}
+
