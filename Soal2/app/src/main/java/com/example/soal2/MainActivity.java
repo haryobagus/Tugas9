43bb0b8c93ff582b78e1c13d1f1962079f9b2cd4
haryobagus <haryobagus14@gmail.com>
2019-10-28 18:55:44 -0600

    soal 2

--- Soal2/app/src/main/java/com/example/soal2/MainActivity.java
+++ Soal2/app/src/main/java/com/example/soal2/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.soal2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.soal2.adapters.SuperHeroAdapter;
+import com.example.soal2.models.SuperHero;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private int[] images ={R.drawable.flash, R.drawable.batman};
+
+    List<SuperHero> listSuperHero = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        SuperHero hero = new SuperHero("Superman");
+        listSuperHero.add(hero);
+        hero = new SuperHero("Superman");
+        listSuperHero.add(hero);
+        hero = new SuperHero("Superman");
+        listSuperHero.add(hero);
+        SuperHeroAdapter superHeroAdapter = new SuperHeroAdapter(listSuperHero);
+        recyclerView.setAdapter(superHeroAdapter);
+        recyclerView.setLayoutManager ( new LinearLayoutManager(this ,LinearLayoutManager.HORIZONTAL, false));
     }
 }
---
+++ Soal2/app/src/main/java/com/example/soal2/adapters/SuperHeroAdapter.java
@@ -0,0 +1,56 @@
+package com.example.soal2.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.soal2.R;
+import com.example.soal2.models.SuperHero;
+
+import java.util.List;
+
+public class SuperHeroAdapter extends RecyclerView.Adapter<SuperHeroAdapter.MyViewHolder> {
+    List<SuperHero> heroList;
+
+    public SuperHeroAdapter(List<SuperHero> heroList) {
+        this.heroList = heroList;
+    }
+
+    @NonNull
+    @Override
+    public SuperHeroAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View superHeroView = layoutInflater.inflate(R.layout.item_super_hero,parent,false);
+        MyViewHolder viewHolder = new MyViewHolder(superHeroView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SuperHeroAdapter.MyViewHolder holder, int position) {
+        //ambil satu item hero
+        SuperHero hero = heroList.get(position);
+        //set text heroName berdasarkan data dari model hero
+        holder.Nama.setText(hero.getHeroName());
+    }
+
+    @Override
+    public int getItemCount()    {
+        return (heroList != null) ? heroList.size() : 0;
+
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView Nama;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            Nama = itemView.findViewById(R.id.nama);
+        }
+    }
+}
+
